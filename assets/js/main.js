9e3f051a5efb832e3e60585ddef557623792566a
RandyMejiaArias <rdmejia3@espe.edu.ec>
2020-03-31 03:35:04 -0500

    Boletines terminado

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+//+542974187527
+
+//Obtener los elementos de la clase '.close'
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+links.forEach(function(link){
+
+  //Agregar un evento click a cada elemento
+  link.addEventListener("click",function(ev){
+    ev.preventDefault();
+
+    //Cambiar la animación del elemento '.content'
+    let content = document.querySelector('.content');
+
+    //Quitarle las clases de animación que ya tiene
+    content.classList.remove("jackInTheBox");
+    content.classList.remove("animated");
+
+    //Agregar clases para animar su salida
+    content.classList.add("animated");
+    content.classList.add("fadeOutUp");
+
+    setTimeout(function(){
+      //Redireccionar nuevamente
+      location.href = "/";
+    },600);
+  });
+});
